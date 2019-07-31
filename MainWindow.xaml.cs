447670f0d8737762faef7aad50f69fe7f6ff5a3d
James Wardle <ac6624@coventry.ac.uk>
2019-07-31 10:34:05 +0100

    SQL Connect -Still requires DB Details Basic Login Form

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
 
 namespace _122IT_CW2_APP_V2
 {
@@ -20,9 +22,62 @@ namespace _122IT_CW2_APP_V2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string conn;
+        private MySqlConnection connect;
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void db_connection()
+        {
+            try
+            {
+                conn = "Server=**; Database=**; Uid=**; Pwd=;"; //Check SRV setting, DB Name, UID and PWD. Also, this is not secure...
+            }
+            catch (MySqlException e)
+            {
+                throw;
+            }
+        }
+        private bool validate_login(string usrnm, string pwd)
+        {
+            db_connection();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandText = "select * from usrs where username=@usrmn and password=@pwd";
+            cmd.Parameters.AddWithValue("@usrnm", usrnm);
+            cmd.Parameters.AddWithValue("@pwd", pwd);
+            cmd.Connection = connect;
+            MySqlDataReader login = cmd.ExecuteReader();
+            if (login.Read())
+            {
+                connect.Close();
+                return true;
+            }
+            else
+            {
+                connect.Close();
+                return false;
+            }
+        }
+
+        private void BTN_LGIN_Click(object sender, RoutedEventArgs e)
+        {
+            string usrnm = USRNM_INPUT.Text;
+            string pwd = PSWD_INPUT.Text;
+            if (usrnm == ""||pwd == "")
+            {
+                MessageBox.Show("Expty Fields");
+                return;
+            }
+            bool r = validate_login(usrnm, pwd);
+            if (r)
+                MessageBox.Show("Correct Login Details");
+            else
+                MessageBox.Show("Incorrect Login Details");
+
+
         }
     }
 }
